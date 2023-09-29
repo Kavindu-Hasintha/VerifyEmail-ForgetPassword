451691292fb55d92fb063d5bc5978c6ad8f00714
Kavindu-Hasintha <mkavinduh@gmail.com>
2023-09-29 18:08:15 +0530

    Add user table

---
+++ VerifyEmailAndForgetPassword/VerifyEmailAndForgetPassword/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VerifyEmailAndForgetPassword.Models;
+
+namespace VerifyEmailAndForgetPassword.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-VRF2530\\MSSQL2022;Initial Catalog=verifyEmailDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;");
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ VerifyEmailAndForgetPassword/VerifyEmailAndForgetPassword/Models/UserRegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VerifyEmailAndForgetPassword.Models
+{
+    public class UserRegisterRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        // string.Empty = by default value will be string empty value
+        [Required, MinLength(6), MaxLength(24)]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
+        public string confirmPassword { get; set; } = string.Empty;
+    }
+}
