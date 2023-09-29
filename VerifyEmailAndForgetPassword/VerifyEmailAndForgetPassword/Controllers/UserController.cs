e3ac41cf4a54fa432aaa615c4a79a7c0bef0d974
Kavindu-Hasintha <mkavinduh@gmail.com>
2023-09-29 19:51:56 +0530

    Add register API

---
+++ VerifyEmailAndForgetPassword/VerifyEmailAndForgetPassword/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using VerifyEmailAndForgetPassword.Data;
+using VerifyEmailAndForgetPassword.Models;
+
+namespace VerifyEmailAndForgetPassword.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+            if (request == null)
+            {
+                return BadRequest();
+            }
+
+            if (_context.Users.Any(u => u.Email == request.Email))
+            {
+                return BadRequest("User already exists.");
+            }
+
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            var user = new User
+            {
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                VerificationToken = CreateRandomToken()
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok("Registration success");
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512()) 
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                // If you use this algorithm with or without Salt everytime
+                // then same password will give same passwordHash
+                // Hackers can get the passwordHash and get plain password
+                // It is not secure.
+
+                // But using passwordSalt with hmac.Key(random key) result is
+                // passwordHash will be different even password is same
+                // Why we store salt - to verify the given password is correct
+            }
+        }
+
+        private string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+    }
+}
--- VerifyEmailAndForgetPassword/VerifyEmailAndForgetPassword/Models/UserRegisterRequest.cs
+++ VerifyEmailAndForgetPassword/VerifyEmailAndForgetPassword/Models/UserRegisterRequest.cs
@@ -7,7 +7,7 @@ namespace VerifyEmailAndForgetPassword.Models
         [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
         // string.Empty = by default value will be string empty value
-        [Required, MinLength(6), MaxLength(24)]
+        [Required, MinLength(6, ErrorMessage = "Please enter at least 6 characters"), MaxLength(24)]
         public string Password { get; set; } = string.Empty;
         [Required, Compare("Password")]
         public string confirmPassword { get; set; } = string.Empty;
